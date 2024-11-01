8f8178c721256df194fce8bace10fbbbde66fab3
billxu <billxu0521@gmail.com>
2024-11-01 10:05:37 +0800

    Update footer-one.vue

--- components/footer/footer-one.vue
+++ components/footer/footer-one.vue
@@ -7,7 +7,7 @@
             <div class="col-xl-3 col-lg-4 footer-intro mb-30">
               <div class="logo mb-35 md-mb-20">
                 <nuxt-link href="/">
-                  <img src="/images/logo/logo_02.svg" alt="">
+                  <img src="/images/logo/logo_02.png" alt="">
                 </nuxt-link>
               </div> 
               <!-- logo -->
