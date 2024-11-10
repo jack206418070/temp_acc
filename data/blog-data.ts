871d4fc2e3d55c797e199728fefa6831e220ba91
billxu <billxu0521@gmail.com>
2024-11-10 21:50:23 +0800

    Create blog-data.ts

---
+++ data/blog-data.ts
@@ -0,0 +1,50 @@
+import type { IMenu } from "@/types/menu-d-t";
+
+const menu_data:IMenu[] = [
+  {
+    id:0,
+    link:'/',
+    title:'首頁'
+  },
+  {
+    id:1,
+    link:'/about-us',
+    title:'計劃簡介'
+  },
+  {
+    id:2,
+    link:'/news',
+    title:'最新消息'
+  },
+  {
+    id:3,
+    link:'/services',
+    title:'服務介紹'
+  },
+  {
+    id:4,
+    link:'/join-us',
+    title:'我想使用服務'
+  },
+  {
+    id:5,
+    link:'/join-us-unit',
+    title:'我想成為試辦單位'
+  },
+  {
+    id:6,
+    link:'/contact',
+    title:'聯絡我們'
+  },
+
+
+  {
+    id:9,
+    link:'/links',
+    title:'相關網站連結'
+  },
+
+
+]
+
+export default menu_data;
