5c444825da31e01f12b6144d532625b7063c84eb
billxu <billxu0521@gmail.com>
2024-11-22 22:34:40 +0800

    update
    調整圖片間距

--- components/hero-banner/hero-banner-empty-index.vue
+++ components/hero-banner/hero-banner-empty-index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block-feature-one position-relative light-bg-deep  lg-mt-80 pt-110 lg-pt-60  lg-pb-60">
+  <div class="block-feature-one position-relative light-bg-deep pt-180 lg-pt-60  lg-pb-60">
     <div class="row">
       <img src="/images/bg_1.png" alt="" class="lazy-img "/>
     </div>
