b08713737f3492e3da4f52df91417cce205a5ace
billxu <billxu0521@gmail.com>
2024-11-25 21:19:01 +0900

    update
    修正tag錯誤

--- components/footer/footer-two.vue
+++ components/footer/footer-two.vue
@@ -10,7 +10,7 @@
               </nuxt-link>
             </div> 
             <!-- logo -->
-            <p class="text-white lh-sm mb-35">勞動部勞動力發展署<bn> 24219 新北市新莊區中平路439號南棟4樓 <bn> (02)8995-6000 <bn>服務時間：週一至週五 08:30~12:30，13:30~17:30 </p>
+            <p class="text-white lh-sm mb-35">勞動部勞動力發展署<br> 24219 新北市新莊區中平路439號南棟4樓 <br> (02)8995-6000 <br>服務時間：週一至週五 08:30~12:30，13:30~17:30 </p>
             <ul class="style-none d-flex align-items-center social-icon">
               <li><a href="#"><i class="bi bi-facebook"></i></a></li>
               <li><a href="#"><i class="bi bi-dribbble"></i></a></li>
