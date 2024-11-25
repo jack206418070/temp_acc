308af93704a33c2d23fee76af10b08b1f035de5f
Sakasky <33936282+Sakasky@users.noreply.github.com>
2024-11-26 01:29:56 +0800

    Update faq.vue

--- pages/faq.vue
+++ pages/faq.vue
@@ -3,7 +3,7 @@
     <!-- breadcrumb area start -->
     <breadcrumb-two
         title="常見問題"
-        subtitle="為您解答關於多元陪伴照顧服務試辦計畫服務的各項疑問"
+        subtitle=""
         page="常見問題"
         col="col-xl-7"
       />
