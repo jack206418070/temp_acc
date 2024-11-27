b1fd9563244718a5c4ccaf014c8f25e80436a33d
Sakasky <33936282+Sakasky@users.noreply.github.com>
2024-11-27 22:52:32 +0800

    Update service-details-area.vue

--- components/service/service-details-area.vue
+++ components/service/service-details-area.vue
@@ -5,15 +5,15 @@
             <div class="">
 				<div class="details-meta ps-xxl-5 ps-xl-3">
 					<div class="reminder-block">
-						<h3 class="reminder-title mb-4">給服務使用者的小叮嚀</h3>
+						<h3 class="reminder-title mb-4"><strong>給服務使用者的小叮嚀</strong></h3>
 						<p class="reminder-intro mb-4">
-							面對「陪伴照顧服務工作者」，提醒您注意以下事項，相信有助於勞顧溝通與照顧品質：
+							面對「陪伴照顧服務工作者」，提醒您注意以下事項，相信有助於溝通與照顧品質：
 						</p>
 						<ul class="reminder-list">
-							<li>交接被照顧者的重要資訊或注意事項，例如用藥、飲食習慣、喜好等。</li>
-							<li>依據被照顧者生活作息，勞僱事前共同訂定用餐、如廁、復健、休息等照顧時間表。</li>
-							<li>尊重不同飲食文化與信仰。例如穆斯林因信仰關係不食用豬肉。</li>
-							<li>提供必要休息時間與能安心休息的空間。</li>
+							<li><strong>交接被照顧者的重要資訊或注意事項</strong>，例如用藥、飲食習慣、喜好等。 </li>
+							<li><strong>依據被照顧者生活作息，事前共同訂定</strong>用餐、如廁、復健、休息等照顧時間表。 </li>
+							<li><strong>尊重不同飲食文化與信仰</strong>。例如穆斯林因信仰關係不食用豬肉。</li>
+							<li><strong>提供必要休息時間與能安心休息的空間</strong>。 </li>
 						</ul>
 						<p class="reminder-note mt-4">
 							<span class="text-primary">★</span> 若有任何問題，可致電各試辦單位，他們將會即時提供協助。
