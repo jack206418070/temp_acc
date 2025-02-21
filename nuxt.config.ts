945f5d45e57c7b109d41f6103480adb74e39b65f
GinJack <jack90928@gmail.com>
2025-02-22 01:27:36 +0800

    將 bootstrap 轉移到 js 靜態檔案 避免 CSP 阻擋外部連結

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       script: [
         {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js",
+          src: "/js/bootstrap.bundle.min.js",
         },
       ],
     }
